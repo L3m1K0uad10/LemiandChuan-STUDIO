a40c3d4e0acea6a2db8c8d92f795618fa60cbe4c
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-11-20 22:26:50 +0530

    updated files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,23 +19,24 @@ function HomePage() {
                 </ul>
             </nav>
 
-            <section className = "homepage-hero-section" style = {{width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", gap: "30px"}}>
+            <section className = "homepage-hero-section" style={{position: "relative", height: "100%", width: "100%", paddingTop: "40px", display: "flex", flexDirection: "column", justifyContent: "center", /* gap: "30px" */}}>
                 <h3 style = {{width: "580px"}}>Through intuitive design, immersive visuals, and meaningful storytelling, we create experiences that people don't just see -- they truly feel.</h3>
 
-                <div style = {{width: "780px", backgroundColor: "white", padding: "30px", borderRadius: "20px"}}>
-                    <div style = {{width: "580px"}}>
-                        <h1 style = {{color: "black", margin: "0px", fontSize: "46px"}}>We Craft Digital Experiences That Matter</h1>
+                <div style={{position: "relative", width: "100%", /* maxWidth: "900px", *//* height: "400px", */display: "flex", alignItems: "center"}}>
+                    <div style={{width: "70%", backgroundColor: "white", padding: "30px", borderRadius: "20px", position: "relative", zIndex: 2, marginTop: "30px"}}>
+                        <div style = {{width: "580px"}}>
+                            <h1 style={{ color: "black", margin: 0, fontSize: "46px" }}>We Craft Digital Experiences That Matter</h1>
+                        </div>
+                        <p style = {{width: "450px", color: "black"}}>
+                            We Blend Creativity, Emotion, And Innovation To Create 
+                            Digital Worlds That Your Audience Can Connect With.
+                        </p>
+                        <button style= {{borderRadius: "20px", backgroundColor: "#0A15A7", fontStyle: "bold"}}>
+                            Request a service
+                        </button>
                     </div>
-                    <p style = {{width: "450px", color: "black"}}>
-                        We Blend Creativity, Emotion, And Innovation To Create 
-                        Digital Worlds That Your Audience Can Connect With.
-                    </p>
-                    <button style= {{borderRadius: "20px", backgroundColor: "#0A15A7", fontStyle: "bold"}}>
-                        Request a service
-                    </button>
+                    <img src={Brain} alt="blue brain" style={{position: "absolute", right: "-1%", bottom: "25%", width: "40%", height: "auto", zIndex: 3, transform: "translate(0, 0)"}}/>
                 </div>
-
-                <img src = {Brain} alt = "hero blue brain" height = "500px" width = "auto" style = {{position: "absolute", bottom: "100px", left: "550px"}}/>
 
                 <div style = {{marginTop: "20px", display: "flex", gap: "10px", marginLeft: "auto"}}>
                     <button style= {{padding: "10px", border: "1px solid gray", borderRadius: "20px", fontSize: "12px"}}>UI/UX</button>
