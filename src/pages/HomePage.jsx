4224b4d401f41b320f917fb9549162734f2b9d0a
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-11-28 21:18:22 +0530

    updated/added files - finished homepage animations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import Brain from "../assets/brain-blue.png";
 import BrainGif from "../assets/brain.gif";
 import Navbar from "../components/Navbar";
+import "../styles/HomePage.css";
 
 
 
 function HomePage() {
     return(
-        <div className = "homepage-container" style = {{display: "flex", flexDirection: "column", gap: "auto", height: "100%"}}>
+        <div className = "homepage-container">
             <Navbar />
 
-            <section className = "homepage-hero-section" style={{position: "relative", height: "100%", width: "100%", paddingTop: "40px", display: "flex", flexDirection: "column", justifyContent: "center", /* gap: "30px" */}}>
-                <h3 style = {{width: "580px"}}>Through intuitive design, immersive visuals, and meaningful storytelling, we create experiences that people don't just see -- they truly feel.</h3>
+            <section className = "homepage-hero-section">
+                <h3>Through intuitive design, immersive visuals, and meaningful storytelling, we create experiences that people don't just see -- they truly feel.</h3>
 
                 <div style={{position: "relative", width: "100%", /* maxWidth: "900px", *//* height: "400px", */display: "flex", alignItems: "center"}}>
                     <div style={{width: "70%", backgroundColor: "white", padding: "30px", borderRadius: "20px", position: "relative", zIndex: 2, marginTop: "30px"}}>
@@ -30,10 +31,10 @@ function HomePage() {
                     <img src={BrainGif} alt="blue brain" style={{position: "absolute", right: "-1%", bottom: "25%", width: "40%", height: "auto", zIndex: 3, transform: "translate(0, 0)"}}/>
                 </div>
 
-                <div style = {{marginTop: "20px", display: "flex", gap: "10px", marginLeft: "auto"}}>
-                    <button style= {{padding: "10px", border: "1px solid gray", borderRadius: "20px", fontSize: "12px"}}>UI/UX</button>
-                    <button style= {{padding: "10px", border: "1px solid gray", borderRadius: "20px", fontSize: "12px"}}>DEVELOPEMENT</button>
-                    <button style= {{padding: "10px", border: "1px solid gray", borderRadius: "20px", fontSize: "12px"}}>DATA SCIENCE</button>
+                <div className = "service-tags-container">
+                    <button>UI/UX</button>
+                    <button>DEVELOPEMENT</button>
+                    <button>DATA SCIENCE</button>
                 </div>
             </section>
         </div>
